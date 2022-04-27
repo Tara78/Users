e8f22f5709f1dc9d1ecf51230ba40663eda7473d
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-04-27 15:37:17 +0200

    Update index.js
    JS

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ let data = [];
 // Fetch random users from API 
 getRandomUser();
 
-
 async function getRandomUser(){
   const res = await fetch("https://randomuser.me/api"); 
   const data = await res.json();
@@ -22,27 +21,14 @@ const user= data.results[0];
 
 // Generating Users 
 const newUser= {
-  name:`${user.name.first} ${user.name.last}`, 
-  money:Math.floor(Math.random()*1000000)
-};
-
-console.log(newUser);
+  name:`${user.name.first} ${user.name.last}`};
+  
 addData(newUser);
 }
 // // Add new obj to Data Array ( by using push Method)
 function addData(obj){
-  data.push(obj);
+data.push(obj);
 updateDOM()
-}
-
-// Doubel Money function 
-function doubleMoney(){
-  data= data.map((user)=>{
-    //(using spread oprator to copy all data we have in user obj line 50 /
-    return { ...user, money: user.money * 2}
-  }); 
-  //   call updateDom() inorder to change 
-  updateDOM()
 }
 
 
@@ -54,20 +40,14 @@ function updateDOM(providedData = data){
   providedData.forEach(item => {
     const elemnt = document.createElement("div");
     elemnt.classList.add("person");
-    elemnt.innerHTML =`<strong>${item.name}</strong>${formatMoney(item.money)}`;
+  
     main.appendChild(elemnt); 
   }); 
 
 }
 
-// Format number to the money ( as currency) (https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string)
-
-function formatMoney(num){
-  return "$ "+ num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-
-}
 
 // Add Event Listeners ( click & function )
 addUserBTN.addEventListener('click', getRandomUser); 
-doubleBTN.addEventListener('click', doubleMoney); 
-sortBTN.addEventListener('click',sort); 
+// doubleBTN.addEventListener('click', doubleMoney); 
+// sortBTN.addEventListener('click', richestPeople); 
